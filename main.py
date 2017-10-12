60e68e3d7d6fa4bc0a026802aaf4189d2386ef10
Kohle <me@kohle.io>
2017-10-11 23:49:09 -0400

    Don't judge
    I wanted to get something committed ;p

---
+++ main.py
@@ -0,0 +1,24 @@
+####################
+#   PY-BLACKJACK   #
+#                  #
+#    PROJECT BY    #
+#   KOHLE FEELEY   #
+####################
+
+## THIS FIRST FILE IS JUST TO GET THE MAIN FUNCTION WORKING
+
+# Import randint
+from random import randint
+
+# Introduce the program
+print("#####################")
+print("#     BLACKJACK     #")
+print("#####################")
+print("#                   #")
+
+# Ask if the player wants to play
+playing = input("#  [P]LAY [S]CORES  #")
+
+while playing.lower() == "p" :
+    print("Playing")
+    break
