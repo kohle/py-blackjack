b437b6a2868fb46e813706648a78fcdb2d27e4d6
Kohle <me@kohle.io>
2017-10-13 01:26:37 -0400

    An updated barebones
    Changed the header and came up with a style guide. No actual game functionality.

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-####################
-#   PY-BLACKJACK   #
-#                  #
-#    PROJECT BY    #
-#   KOHLE FEELEY   #
-####################
+# About this project:
+# py-blackjack is a simple, text-based game of blackjack
+# that is being developed in conjunction with the methods
+# and functionality being taught in CIT-135.
+#
+# Developed by Kohle Feeley
+# BTV 2017
 
-## THIS FIRST FILE IS JUST TO GET THE MAIN FUNCTION WORKING
 
-# Import randint
+# Imports
 from random import randint
 
 # Introduce the program
-print("#####################")
-print("#     BLACKJACK     #")
-print("#####################")
-print("#                   #")
+print("###########################")
+print("#                         #")
+print("#        BLACKJACK        #")
+print("#                         #")
+print("###########################")
 
 # Ask if the player wants to play
-playing = input("#  [P]LAY [S]CORES  #")
+playing = input("\n    PRESS ENTER TO PLAY\n")
 
-while playing.lower() == "p" :
+while playing == "" :
+    print("###########################\n")
     print("Playing")
     break
+
+print("\n###########################\n")
+print("      HAVE A NICE DAY!")
