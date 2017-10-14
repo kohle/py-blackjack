60c4d8437261610aa660b6401d7bbe28b0ff96c1
Kohle <me@kohle.io>
2017-10-14 00:04:26 -0400

    *record scratch* here we go
    Totally just started from blank for simplicity. Core functionality (build a deck, deal hands, and count totals) are written. No gameplay yet.

--- main.py
+++ main.py
@@ -1,29 +1,69 @@
-# About this project:
-# py-blackjack is a simple, text-based game of blackjack
-# that is being developed in conjunction with the methods
-# and functionality being taught in CIT-135.
-#
 # Developed by Kohle Feeley
-# BTV 2017
-
+# Burlington, Vermont 2017
 
 # Imports
-from random import randint
+import random
 
-# Introduce the program
-print("###########################")
-print("#                         #")
-print("#        BLACKJACK        #")
-print("#                         #")
-print("###########################")
+# Create the cards in the deck
+# Since ace is either a 1 or 11 it is easier to go in JQKA order
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
 
-# Ask if the player wants to play
-playing = input("\n    PRESS ENTER TO PLAY\n")
+# Method that deals 2 cards into a hand (a list), remove from deck
+def deal(deck) :
+    hand = []
 
-while playing == "" :
-    print("###########################\n")
-    print("Playing")
-    break
+    for x in range(2) :
+        random.shuffle(deck)
+        card = deck.pop()
 
-print("\n###########################\n")
-print("      HAVE A NICE DAY!")
+        # If we pick a JQKA, change it from a number to a string for the list
+        if card == 11 :
+            card = "J"
+        if card == 12 :
+            card = "Q"
+        if card == 13 :
+            card = "K"
+        if card == 14 :
+            card = "A"
+
+        hand.append(card)
+
+    return hand
+
+# Method to calculate the hand's total value
+def total(hand) :
+    total = 0
+
+    for card in hand :
+        if card == "J" or card == "Q" or card == "K" :
+            total = total + 10
+        elif card == "A" :
+            if total >= 11 :
+                total = total + 1
+            else :
+                total = total + 11
+        else :
+            total = total + card
+
+    return total
+
+# BY JOVE IT WORKS: 
+# Deal the house and the player
+dealer = deal(deck)
+player = deal(deck)
+
+# Get the totals
+dealer_total = total(dealer)
+player_total = total(player)
+
+# Print the results
+print("Dealer\'s hand:", str(dealer), "Total:", str(total(dealer)))
+print("Player\'s hand:", str(player), "Total:", str(total(player)))
+
+# Compare the scores and declare a winner
+if dealer_total > player_total :
+    print("Dealer wins!")
+elif player_total > dealer_total :
+    print("Player wins!")
+elif player_total == dealer_total :
+    print("It\'s a tie!")
