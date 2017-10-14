fb7a024a604ed650d99968bdf2eaba265b52ba1b
Kohle Feeley <me@kohle.io>
2017-10-14 12:56:57 -0400

    Hitting and winner
    Function that hits the given hand as well as a function that determines the winner and prints the appropriate statement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 
 # Create the cards in the deck
 # Since ace is either a 1 or 11 it is easier to go in JQKA order
-deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
 
 # Method that deals 2 cards into a hand (a list), remove from deck
 def deal(deck) :
@@ -30,6 +30,24 @@ def deal(deck) :
 
     return hand
 
+# Method to hit, adds another card to the specified hand
+def hit(hand) :
+    card = deck.pop()
+
+    # If we pick a JQKA, change it from a number to a string for the list
+    if card == 11 :
+        card = "J"
+    if card == 12 :
+        card = "Q"
+    if card == 13 :
+        card = "K"
+    if card == 14 :
+        card = "A"
+
+    hand.append(card)
+
+    return hand
+
 # Method to calculate the hand's total value
 def total(hand) :
     total = 0
@@ -47,23 +65,35 @@ def total(hand) :
 
     return total
 
-# BY JOVE IT WORKS: 
+# Method to determime who won and print the appropriate statement
+def winner() :
+    if dealer_total > 21 :
+        print("The dealer busted! You win!")
+    elif player_total > 21 :
+        print("You busted! Dealer wins!")
+    elif dealer_total > player_total :
+        print("Dealer wins!")
+    elif player_total > dealer_total :
+        print("Player wins!")
+    elif player_total == dealer_total :
+        print("It\'s a tie!")
+
+# Smiple testing procedure; NOT a working game:
 # Deal the house and the player
 dealer = deal(deck)
 player = deal(deck)
-
-# Get the totals
-dealer_total = total(dealer)
-player_total = total(player)
 
 # Print the results
 print("Dealer\'s hand:", str(dealer), "Total:", str(total(dealer)))
 print("Player\'s hand:", str(player), "Total:", str(total(player)))
 
-# Compare the scores and declare a winner
-if dealer_total > player_total :
-    print("Dealer wins!")
-elif player_total > dealer_total :
-    print("Player wins!")
-elif player_total == dealer_total :
-    print("It\'s a tie!")
+# Hit the player
+player = hit(player)
+print("Player\'s hand:", str(dealer), "Total:", str(total(dealer)))
+
+# Get the totals
+dealer_total = total(dealer)
+player_total = total(player)
+
+# Winner
+winner()
